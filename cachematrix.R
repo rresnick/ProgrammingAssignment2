dd02cf159cfdfb3f93b01e54e87ccb408d2a1268
Ron Resnick <resnick.ron@gmail.com>
2014-07-27 16:19:27 -0500

    First draft commit of "cachematrix.R"
    First incomplete draft of programming assignment to gitHub

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCachMatrix creates a "Matrix" object that preserves a private cached copy of its inverse
+## Functions written by Ron Resnick, July 27 2014 for Coursera "R Programming" course.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ inv <- NULL
+ set <- function(y) {
+   x <<- y
+   inv <<- NULL
+ }
+ get <- function() x
+ setinv <- function(inverse) inv <<- inverse
+ getinv <- function() inv
+ list(set = set, get = get,
+      setinv = setinv,
+      getinv = getinv)
 }
 
 

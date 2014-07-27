e02de56cc1270072c3ff84941371d09a3b83e84b
Ron Resnick <resnick.ron@gmail.com>
2014-07-27 16:43:51 -0500

    Final code, tested and ready for submission
    Two functions - makeCacheMatrix and cacheSolve - fully working, debugged
    and tested.

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## functions do
 
 ## makeCachMatrix creates a "Matrix" object that preserves a private cached copy of its inverse
-## Functions written by Ron Resnick, July 27 2014 for Coursera "R Programming" course.
+## Function written by Ron Resnick, July 27 2014 for Coursera "R Programming" course.
 
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -20,8 +20,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve accepts a "matrix" object as built by makeCacheMatrix. If the inverse had already been
+## solved and cached, it is returned. Otherwise solve() is called with default parameters to
+## invert and cache the matrix, then return the inverse.
+
+## Function written by Ron Resnick, July 27 2014 for Coursera "R Programming" course.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
